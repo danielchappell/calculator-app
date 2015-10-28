888b631723cfbdd149a824704f3aca40bb1bf4e2
Daniel Chappell <daniel.chappell@q2ebanking.com>
2015-10-28 09:39:11 -0500

    refactored calulator internals to not be a crazy ass state machine.

---
+++ tests/unit/utils/math-test.js
@@ -0,0 +1,15 @@
+/* jshint expr:true */
+import { expect } from 'chai';
+import {
+  describe,
+  it
+} from 'mocha';
+import math from 'calculator-app/utils/math';
+
+describe('math', function() {
+  // Replace this with your real tests.
+  it('works', function() {
+    var result = math();
+    expect(result).to.be.ok;
+  });
+});

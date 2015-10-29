e639cd7b2dd1f3c220c4c734cc2f1dba942355ee
Kevin Boucher <kboucher@q2ebanking.com>
2015-10-29 17:41:51 -0500

    Handle active operations and screen overflow. Replace erroneously removed error message code

--- app/components/calc-button.js
+++ app/components/calc-button.js
@@ -3,7 +3,7 @@ import Ember from 'ember';
 export default Ember.Component.extend({
     isDisabled: false,
     tagName: 'button',
-    classNames: ['btn-flat'],
+    classNames: ['btn-flat', 'waves-effect', 'waves-light'],
     classNameBindings: ['isDisabled:disabled'],
     attributeBindings: ['isDisabled:disabled'],
     click() {

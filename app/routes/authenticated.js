4d288ce12fddb6e7bbe95acdf96c120c7f097910
Daniel Chappell <daniel.chappell@q2ebanking.com>
2015-10-29 13:46:50 -0500

    return promise from beforeModel when checking authentication

--- app/routes/authenticated.js
+++ app/routes/authenticated.js
@@ -3,7 +3,7 @@ import Ember from 'ember';
 export default Ember.Route.extend({
     session: Ember.inject.service(),
     beforeModel(transition) {
-        this.get('session').checkAuth().catch(() => {
+        return this.get('session').checkAuth().catch(() => {
             transition.abort();
             this.set('session.attemptedTransition');
             this.transitionTo('login');

96f61881524d7b626e2a5a9aa7603650fc979b66
Daniel Chappell <daniel.chappell@q2ebanking.com>
2015-10-30 09:26:20 -0500

    chang password requirements because why

--- app/models/user.js
+++ app/models/user.js
@@ -5,10 +5,10 @@ export default DS.Model.extend({
     username: DS.attr(),
     password: DS.attr(),
     isUsernameValid: function() {
-        return this.get('username') && this.get('username').length > 5;
+        return this.get('username') && this.get('username').length > 4;
     }.property('username'),
     isPasswordValid: function() {
-        return this.get('password') && this.get('password').length > 8;
+        return this.get('password') && this.get('password').length > 4;
     }.property('password'),
     isValid: Ember.computed.and('isUsernameValid', 'isPasswordValid'),
     isNotValid: Ember.computed.not('isValid')
